b5ffe38646e6a8156e2b1eaa51d0361e94dc5d2e
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-12-11 13:38:12 +0100

    solved template issue

--- src/app/lateral-bar/lateral-bar.component.ts
+++ src/app/lateral-bar/lateral-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TemplateComponent } from '../template/template.component';
 
 @Component({
   selector: 'app-lateral-bar',
   standalone: true,
-  imports: [RouterOutlet, TemplateComponent],
+  imports: [RouterOutlet],
   templateUrl: './lateral-bar.component.html',
   styleUrl: './lateral-bar.component.css'
 })
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { LateralBarComponent } from '../lateral-bar/lateral-bar.component';
-import { TemplateComponent } from '../template/template.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [LateralBarComponent,TemplateComponent],
+  imports: [LateralBarComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
