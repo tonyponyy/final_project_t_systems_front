13ae3bff8ea294c6f8e466a7b9509b94759bb978
AsierGArranz <asiergarranz@gmail.com>
2023-12-12 18:21:37 +0100

    diseño y rutas

--- src/app/lateral-bar/lateral-bar.component.ts
+++ src/app/lateral-bar/lateral-bar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-lateral-bar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './lateral-bar.component.html',
   styleUrl: './lateral-bar.component.css'
 })
