0de3242deb5b94057b46e0df13f1e85339921fd5
adriii28 <adrigallego28@gmail.com>
2023-12-19 13:33:21 +0100

    change state

--- src/app/edit-interview/edit-interview.component.ts
+++ src/app/edit-interview/edit-interview.component.ts
@@ -1,12 +1,123 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { LateralBarComponent } from '../lateral-bar/lateral-bar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RequestService } from '../service/request.service';
+import { InterviewSkillsComponent } from "../modals/interview-skills/interview-skills.component";
 
 @Component({
-  selector: 'app-edit-interview',
-  standalone: true,
-  imports: [],
-  templateUrl: './edit-interview.component.html',
-  styleUrl: './edit-interview.component.css'
+    selector: 'app-edit-interview',
+    standalone: true,
+    templateUrl: './edit-interview.component.html',
+    styleUrl: './edit-interview.component.css',
+    imports: [LateralBarComponent, ReactiveFormsModule, InterviewSkillsComponent, RouterModule]
 })
 export class EditInterviewComponent {
+  interviewId : number = 0;
+  interview : any = null;
+  arraySkills : any[] = [];
+  editInterview : FormGroup
+  protected class :string[] = ["is-success","is-primary","is-link","is-info","is-warning","is-danger"];
 
+  constructor(private ruta: ActivatedRoute, private formBuilder: FormBuilder, private request: RequestService, private router: Router){
+    this.editInterview = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      end_date: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void{
+    console.log("on init character");
+    this.ruta.paramMap.subscribe(params => {
+      const idFromParams = params.get('id');
+      const idAsNumber = Number(idFromParams);
+
+      
+      if (idFromParams !== null) {
+        this.interviewId = idAsNumber;
+
+        this.request.get_hr_interview(this.interviewId).subscribe((data : any) => {
+          this.interview = data.interview;
+          console.log(this.interview);
+          this.arraySkills = this.interview.skills
+          const originalEndDate = new Date(this.interview.end_date);
+          const formattedEndDate = originalEndDate.toISOString().split('T')[0];
+
+          this.editInterview = this.formBuilder.group({
+            title: [this.interview.title, Validators.required],
+            description: [this.interview.description, Validators.required],
+            end_date: [formattedEndDate , Validators.required],
+          });
+        },
+        error => {
+          console.log('no');
+          
+        })
+        console.log(this.interviewId);
+        
+      } else {
+        console.error('No se encontró el parámetro "id" en la URL.');
+      }
+    });
+  }
+
+  onSubmit(){
+    if (this.editInterview.valid){
+      const interview = this.editInterview.value as Interview;
+      interview.id = this.interviewId;
+
+      console.log(interview);
+      
+      this.request.edit_interview(interview).subscribe((data : any) => {
+        console.log('si');
+        this.addSkillsToInterview(interview)
+      },
+      error => {
+        console.log('error'+error);
+      })
+    }
+  }
+
+  addSkillsToInterview(interview : any){
+    console.log(interview.id);
+    for (let i = 0; i < this.arraySkills.length; i++) {
+      this.request.add_skill_interview(interview.id,this.arraySkills[i].id).subscribe((data :any) => {
+        console.log('funciona');
+      },
+      error => {
+        console.log('error');
+      })
+    }
+
+    this.router.navigate(['/interviews']);
+    
+  }
+
+  deleteSkill(skillDelete:any){
+    const indexToDelete = this.arraySkills.findIndex(skill => skill.id === skillDelete.id);
+
+    if (indexToDelete !== -1) {
+      this.arraySkills.splice(indexToDelete, 1);
+      console.log(this.arraySkills);
+      
+    } else {
+        console.log('error');
+    }
+  }
+
+  recibirArray(array: any[]) {
+    for (let index = 0; index < array.length; index++) {
+      this.arraySkills.push(array[index])
+    }
+    this.arraySkills=[... new Set(this.arraySkills)]
+    console.log(this.arraySkills)
+  }
+}
+
+export class Interview {
+  id?: number;
+  title?: string;
+  description?: string;
+  end_date?: Date;
 }
--- src/app/interview-hr/interview-hr.component.ts
+++ src/app/interview-hr/interview-hr.component.ts
@@ -1,50 +1,113 @@
 import { Component } from '@angular/core';
-import { LateralBarComponent } from "../lateral-bar/lateral-bar.component";
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { LateralBarComponent } from '../lateral-bar/lateral-bar.component';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterModule,
+} from '@angular/router';
 import { RequestService } from '../service/request.service';
 import { DatePipe } from '@angular/common';
+import { state } from '@angular/animations';
 
 @Component({
-    selector: 'app-interview-hr',
-    standalone: true,
-    templateUrl: './interview-hr.component.html',
-    styleUrl: './interview-hr.component.css',
-    imports: [LateralBarComponent,DatePipe]
+  selector: 'app-interview-hr',
+  standalone: true,
+  templateUrl: './interview-hr.component.html',
+  styleUrl: './interview-hr.component.css',
+  imports: [LateralBarComponent, DatePipe, RouterModule],
 })
 export class InterviewHrComponent {
-  constructor(private route: ActivatedRoute,private requestservice: RequestService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private requestservice: RequestService,
+    private router: Router
+  ) {}
 
   protected interview_id: any;
-  protected interview:any
-  protected user_interview:any;
-  protected tests :any;
-  protected loaded :boolean = false;
-  public states :string[] =['You have not passed the process','You are enrolled','First interview','In selection process','Hired'];
-  protected class :string[] = ["is-success","is-primary","is-link","is-info","is-warning","is-danger"];
-
+  protected interview: any;
+  protected user_interview: any;
+  protected tests: any;
+  protected loaded: boolean = false;
+  public states: string[] = [
+    'You have not passed the process',
+    'You are enrolled',
+    'First interview',
+    'In selection process',
+    'Hired',
+  ];
+  protected class: string[] = [
+    'is-success',
+    'is-primary',
+    'is-link',
+    'is-info',
+    'is-warning',
+    'is-danger',
+  ];
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.interview_id = params.get('id');
       this.get_interview(this.interview_id);
     });
-    
-}
-get_interview(id_interview :any) {
-  this.requestservice.get_hr_interview(id_interview).subscribe((data: any) => {
-    console.log(data);
-    this.interview = data.interview;
-    this.user_interview = data.users;
-    this.tests = data.tests;
-    this.loaded = true;
-  });
-}
+  }
+  get_interview(id_interview: any) {
+    this.requestservice
+      .get_hr_interview(id_interview)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.interview = data.interview;
+        this.user_interview = data.users;
+        console.log('userinterview', this.user_interview);
+        this.tests = data.tests;
+        this.loaded = true;
+      });
+  }
 
-edit(id_interview :number){
-console.log("edit -->"+id_interview)
-}
-delete(id_interview :number){
-console.log("delete -->"+id_interview)
-}
+  edit(id_interview: number) {
+    console.log('edit -->' + id_interview);
+  }
 
+  changeState(user: any, item: any) {
+    let idState = this.stateId(item);
+    this.requestservice.change_state(user.id,idState).subscribe((data:any) => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  stateId(item: string): number {
+    switch (item) {
+      case 'You have not passed the process':
+        return 4;
+        break;
+      case 'You are enrolled':
+        return 1;
+        break;
+      case 'First interview':
+        return 3;
+        break;
+      case 'In selection process':
+        return 2;
+        break;
+      case 'Hired':
+        return 5;
+        break;
+        default:
+        return 0 ;
+        break
+    }
+  }
+  delete(id_interview: number) {
+    this.requestservice.delete_interview(id_interview).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.router.navigate(['/interviews']);
+      },
+      (error) => {
+        console.log('error');
+      }
+    );
+  }
 }
